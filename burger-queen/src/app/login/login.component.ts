205562ebb55ab6dc3b5a55b2dc803c95794d5e81
Ale Santos Coz <ale.santoscoz@gmail.com>
2023-04-13 16:47:09 -0500

    ignorar intentos

--- burger-queen/src/app/login/login.component.ts
+++ burger-queen/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+/* import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms'
 
 
@@ -12,4 +12,58 @@ export class LoginComponent{
   user = new FormControl('', Validators.required);
   password = new FormControl('', [Validators.required, Validators.minLength(6)]);
 
+}
+ */
+
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr'
+import { AuthService } from '../service/auth.service';
+import { first } from 'rxjs';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent{
+  formLogin: FormGroup;
+  // private router$: Router;
+  // private builder$: FormBuilder;
+  // private auth$: AuthService;
+
+
+  constructor(
+    private  router: Router,
+    private  builder: FormBuilder,
+    private  toastr: ToastrService,
+    private  auth: AuthService) {
+    // this.router$ = router;
+    // this.builder$ = builder;
+    // this.auth$ = auth;
+
+    this.formLogin = this.builder.group({
+      user: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
+
+  }
+
+  login(){
+    const val = this.formLogin.value;
+
+    
+
+    if(val.user && val.password){
+      this.auth.login(val.user, val.password)
+      .subscribe(
+        () => {
+          this.router.navigateByUrl('home')
+        }
+      )
+    }
+  }
 }
--- burger-queen/src/app/service/auth.service.ts
+++ burger-queen/src/app/service/auth.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private http: HttpClient) { }
+  // apiurl: string = 'http://localhost:8080/login';
+
+  login(user: string, password: string): Observable<string> {
+    return this.http.post<string>('/login', {user, password})
+  }
+
+}
+/* import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
@@ -7,3 +23,4 @@ export class AuthService {
 
   constructor() { }
 }
+ */
